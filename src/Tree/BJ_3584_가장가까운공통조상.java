e181a32debb038eee3b6daace4826cbfdf81dea2
Inyoung.R <inyoung.r@gmail.com>
2022-04-28 23:09:27 +0900

    정기 커밋

---
+++ src/Tree/BJ_3584_가장가까운공통조상.java
@@ -0,0 +1,16 @@
+package Tree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_3584_가장가까운공통조상 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(bf.readLine());
+
+        for(int t=0; t<T; t++){
+            int n = Integer.parseInt(bf.readLine());
+        }
+    }
+}
